745fe717b374349ad404b97c8a1092316a5d8b8a
Dmitry G. Anderson <dandgerson@gmail.com>
2021-05-14 00:35:58 +0300

    clean logs

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import cl from 'clsx'
 
 import s from './App.m.scss'
 
-console.log('asdfsaffsfsafa')
-console.log('asdfsaffsfsafa')
-console.log('asdfsaffsfsafa')
-console.log('asdfsaffsfsafa')
-console.log('asdfsaffsfsafa')
-
 const App = () => (
   <div className={cl(s.root)}>
     <div>Hello Flat World!!!!</div>
