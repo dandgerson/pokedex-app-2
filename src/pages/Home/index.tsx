96fce91b69c70bdc1a18c351aa1a762d85dc51ac
Dmitry G. Anderson <dandgerson@gmail.com>
2021-05-16 21:35:06 +0300

    add paralax

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import cl from 'clsx'
+
+import cloudBig from 'images/cloudBig.png'
+import cloudSmall from 'images/cloudSmall.png'
+import pikachu from 'images/Pikachu.png'
+import pokeball1 from 'images/PokeBall1.png'
+import pokeball2 from 'images/PokeBall2.png'
 
 import Button from 'components/Button'
 
 import s from './Home.m.scss'
 
-const Home = () => (
-  <div className={cl(s.root)}>
-    <div
-      style={{
-        width: '40%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        marginTop: '128px',
-      }}>
-      <div className={cl('h1')}>
-        <span className={cl('bold')}>Find</span> all your favorite <div className={cl('bold')}>Pokemons</div>
+const Home = () => {
+  const [screenX, setSreenX] = useState(0)
+  const [screenY, setSreenY] = useState(0)
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      setSreenX(Math.abs(event.screenX))
+      setSreenY(Math.abs(event.screenY))
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => window.removeEventListener('mousemove', handleMouseMove)
+  }, [screenX, screenY])
+
+  return (
+    <div className={cl(s.root)}>
+      <div className={cl(s.cover)}>
+        <div className={cl('h1', s.title)}>
+          <span className={cl('bold')}>Find</span> all your favorite <span className={cl('bold')}>Pokemon</span>
+        </div>
+
+        <div
+          className={cl('h2', s.title)}
+          style={{
+            paddingRight: '20px',
+          }}>
+          You can know the type of Pokemon, its strengths, disadvantages and abilities
+        </div>
+
+        <Button handleClick={() => console.log('click')} text="See pokemons" />
       </div>
 
       <div
-        className={cl('h3')}
+        className={cl(s.cloudBig)}
         style={{
-          marginTop: '32px',
+          transform: `translate(${screenX * 0.003}%, ${screenY * 0.003}%)`,
         }}>
-        You can know the type of Pokemon, its strengths, disadvantages and abilities
+        <img src={cloudBig} alt="" />
       </div>
 
-      <Button text="See pokemons" handleClick={() => console.log('click!')} />
+      <div
+        className={cl(s.cloudSmall)}
+        style={{
+          transform: `translate(${screenX * 0.003}%, ${screenY * 0.003}%)`,
+        }}>
+        <img src={cloudSmall} alt="" />
+      </div>
+
+      <div
+        className={cl(s.pikachu)}
+        style={{
+          transform: `translate(${screenX * -0.009}%, ${screenY * -0.01}%)`,
+        }}>
+        <img src={pikachu} alt="" />
+      </div>
+
+      <div
+        className={cl(s.pokeball1)}
+        style={{
+          transform: `translate(${screenX * 0.003}%, ${screenY * 0.003}%)`,
+        }}>
+        <img src={pokeball1} alt="" />
+      </div>
+
+      <div
+        className={cl(s.pokeball2)}
+        style={{
+          transform: `translateY(${screenY * -0.005}%)`,
+        }}>
+        <img src={pokeball2} alt="" />
+      </div>
     </div>
-    <div>image</div>
-  </div>
-)
+  )
+}
 
 export default Home
